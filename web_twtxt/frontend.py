ff359d0e804e1fc4206aaa79a129793aab1b0e72
Myles Braithwaite <me@mylesbraithwaite.com>
2016-03-22 11:38:02 -0400

    Did some stuff can't remember.

--- web_twtxt/frontend.py
+++ web_twtxt/frontend.py
@@ -4,14 +4,9 @@ import requests
 from flask import (
     Blueprint,
     render_template,
-    flash,
-    redirect,
-    url_for,
     request,
     current_app as app
 )
-
-from flask_bootstrap import __version__ as FLASK_BOOTSTRAP_VERSION
 
 from .utils import process_tweet
 
@@ -23,14 +18,14 @@ frontend = Blueprint('frontend', __name__)
 def index():
     r = requests.get(request.args.get('feed', app.config['TWTXT_FEED']))
 
-    raw_tweet = r.text
-    
+    raw_tweets = r.text
+
     tweets = []
-    
+
     for raw_tweet in raw_tweets.split('\n')[:-1]:
         try:
             tweets.append(process_tweet(raw_tweet))
         except (ValueError, OverflowError) as e:
             logger.debug(e)
-    
+
     return render_template('index.html', tweets=reversed(tweets))
--- web_twtxt/utils.py
+++ web_twtxt/utils.py
@@ -7,24 +7,27 @@ from twtxt.parser import parse_iso8601
 
 
 def get_links(text):
-    url_regex = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    url_regex = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|'
+                           '(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+
     urls = url_regex.findall(text)
-    
+
     embedly = []
-    
+
     client = Embedly(app.config['EMBEDLY_KEY'])
-    
+
     for url in urls:
         embedly.append(client.oembed(url))
-    
+
     return embedly
+
 
 def process_tweet(raw_tweet):
     tweet = {}
-    
+
     parts = raw_tweet.partition('\t')
     tweet['timestamp'] = parse_iso8601(parts[0])
     tweet['text'] = parts[2].lstrip().rstrip()
     tweet['urls'] = get_links(parts[2].lstrip().rstrip())
-    
+
     return tweet
